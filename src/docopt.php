f417bcb27442e6e57ada430fbc7f60cba34dcd56
1ike <googlohren@gmail.com>
2017-11-20 14:51:33 +0300

    Start!

---
+++ src/docopt.php
@@ -0,0 +1,15 @@
+<?php
+namespace GenDiff;
+
+const DOC = <<<DOC
+Generate diff.
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  --format <fmt>                Report format [default: pretty]
+
+DOC;
